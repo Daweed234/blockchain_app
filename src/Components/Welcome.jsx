5cc3d1d27f8020c9f9c30b3d298a5fe7e2065cfa
David <ntwalidavid31@gmail.com>
2024-05-11 15:40:46 +0200

    some styles added

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { SiEthereum } from "react-icons/si";
+
+import { Loader } from "./";
 
 const Welcome = () => {
   return (
-    <div>Welcome</div>
-  )
-}
+    <div className="flex w-full justify-center items-center">
+      <div className=" flex flex-row md:flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col md:mr-10">
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient">Send Crypto <br /> accross the world</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Welcome
+export default Welcome;
