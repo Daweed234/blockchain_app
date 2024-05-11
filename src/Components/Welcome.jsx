388794cd8f6db1623ee381f5622b5ccb6e4a5451
David <ntwalidavid31@gmail.com>
2024-05-11 17:11:34 +0200

    some styles added

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
+import { BsInfoCircle, BsInfoSquare } from "react-icons/bs";
 import { SiEthereum } from "react-icons/si";
 
 import { Loader } from "./";
 
-const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+const commonStyles =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
+const Input= ({ placeholder, name, type, value, handleChange }) => {
+  <input placeholder={placeholder} 
+  type={type}
+  step="0.0001"
+  value={value}
+  onChange={(e) => handleChange(e, name)}
+  className="my-2 w-full rounded-smp p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism "
+  />;
+};
 
 const Welcome = () => {
   const connectWallet = () => {};
@@ -29,32 +40,42 @@ const Welcome = () => {
             <p className="text-base text-white font-semibold">Connect Wallet</p>{" "}
           </button>
           <div className="grid grid-cols-3 w-full mt-10">
-          <div className={`rounded-tl-2xl ${commonStyles}`}>
-              Reliability
-            </div>
-          <div className={`${commonStyles}`}>
-              Security
-            </div>
-          <div className={`rounded-tr-2xl ${commonStyles}`}>
-              Ethereum
-            </div><div className={`rounded-bl-2xl ${commonStyles}`}>
-            Web 3.0
-            </div>
-          <div className={`${commonStyles}`}>
-              Low Fees
-            </div>
-          <div className={`rounded-br-2xl ${commonStyles}`}>
-            Blockchain
-          </div>
+            <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
+            <div className={`${commonStyles}`}>Security</div>
+            <div className={`rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
+            <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
+            <div className={`${commonStyles}`}>Low Fees</div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
           </div>
         </div>
 
         <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
-          <div>
-            
+          <div className=" p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorpism">
+            <div className="flex justify-between flex-col w-full h-full ">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="white" />
+                </div>
+                <BsInfoSquare fontSize={17} color="white" />
+              </div>
+              <div>
+                <p className="text-white font-light text-sm">Address</p>
+                <p className="text-white font-semibold text-lg mt-1">
+                  Ethereum
+                </p>
+                
+              </div>
+            </div>
           </div>
-        </div>
 
+          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism ">
+          <input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
+          <input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}} />
+          <input placeholder="KeyWord (GIF)" name="keyword" type="text" handleChange={() => {}} />
+          <input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+          </div>
+
+        </div>
       </div>
     </div>
   );
