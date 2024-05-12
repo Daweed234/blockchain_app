47f1e92f98cbf718367e60e1a5850d5e74b55600
David <ntwalidavid31@gmail.com>
2024-05-12 16:54:31 +0200

    some blockchain added

--- smart_contract/contracts/Transaction.sol
+++ smart_contract/contracts/Transaction.sol
@@ -2,16 +2,29 @@
 pragma solidity ^0.8.24;
 
 contract Transaction{
-    uint256 transactionCounter;
+    uint256 transactionCount;
 
     event Transfer(address from, address receiver, uint amount, string message, uint timestamp, string keyword);
 
-    struct transferStruct{
+    struct TransferStruct{
         address sender;
         address reciever;
         uint amount;
         string message;
         uint timestamp;
         string keyword;
+    }
+
+    TransferStruct[] transaction;
+
+    function addToBlockchain ( address payable receiver, uint amount, stirng memory message, string memory keyword ) public {
+        transaction += 1;        
+        transaction.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+    }
+    function getAllTransactions () public view returns (TransferStruct[] memory) {
+        
+    }
+    function getTransactionCount () public view returns (uints256) {
+        
     }
 }
