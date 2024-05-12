adb9ea2580f5c8dac610f48758a794fe34cb2a2e
David <ntwalidavid31@gmail.com>
2024-05-12 13:07:48 +0200

    some changes made

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -8,27 +8,27 @@ import { Loader } from "./";
 const commonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const Input= ({ placeholder, name, type, value, handleChange }) => {
-  <input placeholder={placeholder} 
-  type={type}
-  step="0.0001"
-  value={value}
-  onChange={(e) => handleChange(e, name)}
-  className="my-2 w-full rounded-smp p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism "
+const Input = ({ placeholder, name, type, value, handleChange }) => 
+{
+  <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-smp p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism "
   />;
 };
 
 const Welcome = () => {
   const connectWallet = () => {};
 
-  const handleSubmit = () =>{
-
-  }
+  const handleSubmit = () => {};
 
   return (
     <div className="flex w-full justify-center items-center">
-      <div className=" flex flex-row md:flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col md:mr-10">
+      <div className=" flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient">
             Send Crypto <br /> accross the world
           </h1>
@@ -53,7 +53,7 @@ const Welcome = () => {
           </div>
         </div>
 
-        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
           <div className=" p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorpism">
             <div className="flex justify-between flex-col w-full h-full ">
               <div className="flex justify-between items-start">
@@ -67,38 +67,50 @@ const Welcome = () => {
                 <p className="text-white font-semibold text-lg mt-1">
                   Ethereum
                 </p>
-
               </div>
             </div>
           </div>
 
           <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism ">
-          <input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
-          <input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}} />
-          <input placeholder="KeyWord (GIF)" name="keyword" type="text" handleChange={() => {}} />
-          <input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+            <Input
+              placeholder="Address To"
+              name="addressTo"
+              type="text"
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder="Amount (ETH)"
+              name="amount"
+              type="number"
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder="KeyWord (GIF)"
+              name="keyword"
+              type="text"
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder="Enter Message"
+              name="message"
+              type="text"
+              handleChange={() => {}}
+            />
 
-        <div className="h-[1px] w-full bg-gray-400 my-2">
-
-          {false ? 
-          (<Loader />
-        )
-          :(
-            <button
-            type="button"
-            onClick={handleSubmit}
-            className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-md cursor-pointer"
-            >
-Send Now
-            </button>
-          )
-
-          }
-
-        </div>
-
+            <div className="h-[1px] w-full bg-gray-400 my-2">
+              {true ? (
+                <Loader />
+              ) : (
+                <button
+                  type="button"
+                  onClick={handleSubmit}
+                  className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-md cursor-pointer"
+                >
+                  Send Now
+                </button>
+              )}
+            </div>
           </div>
-
         </div>
       </div>
     </div>
