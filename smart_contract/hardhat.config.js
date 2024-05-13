8f4c6e2b98a0b5a32a1e6fc092ed09d5e54af155
David <ntwalidavid31@gmail.com>
2024-05-13 05:42:49 +0200

    some changes made

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+const { solidity } = require("ethereum-waffle")
+const { network } = require("hardhat")
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.24",
-};
+require("@nomiclabs/hardhat-waffle")
+
+module.export = {
+
+  solidity:'0.8.24',
+  network:{
+    ropsten:{
+      URL:'https://eth-mainnet.g.alchemy.com/v2/wUxenTVl9IqvHe-LEYcgxIngZZ03L86Y/getNFTs/?owner=vitalik.eth', 
+    accounts:'0x29ee36015f5c7542E3696ecF387eB1aB42dD6d59' 
+    }
+  }
+}
